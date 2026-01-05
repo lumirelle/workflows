016e3d0e724b7525fbefbdd1dfad54a0c35591ae
Lumirelle <shabbyacc@outlook.com>
2026-01-05 18:46:00 +0800

    feat(release): use workaround to support trusted publishing

--- setup-bun/publish.sh
+++ setup-bun/publish.sh
@@ -1,4 +1,4 @@
 # As Bun does not support `publish --recursive`, we need to manually publish each package.
 for package in "$@"; do
-  (cd $package && bun publish --access public)
+  (cd $package  && bun pm pack --filename release.tgz && bunx npm publish ./release.tgz --access public)
 done
