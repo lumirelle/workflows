7229f1ee90f7e842382db362a62334f1451010ce
Lumirelle <shabbyacc@outlook.com>
2026-01-05 18:50:29 +0800

    fix(release): path of dist

--- setup-bun/publish.sh
+++ setup-bun/publish.sh
@@ -1,4 +1,4 @@
 # As Bun does not support `publish --recursive`, we need to manually publish each package.
 for package in "$@"; do
-  (cd $package  && bun pm pack --filename release.tgz && bunx npm publish ./release.tgz --access public)
+  (cd $package  && bun pm pack --filename "${package}/release.tgz" && bunx npm publish ./release.tgz --access public)
 done
