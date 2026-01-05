0483ed42b6af0adcb9ae862c75d0e90105a4dcd7
Lumirelle <shabbyacc@outlook.com>
2026-01-05 15:53:48 +0800

    feat(release): try to expand path without ls

--- setup-bun/publish.sh
+++ setup-bun/publish.sh
@@ -1,5 +1,5 @@
 # As Bun does not support `publish --recursive`, we need to manually publish each package.
-for package in "$(ls $1)"; do
+for package in "$1"; do
   # (cd $package && bun publish --access public)
   echo $package
 done
