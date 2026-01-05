d7ef56be7e60e59f405155b63e754c1553a7305c
Lumirelle <shabbyacc@outlook.com>
2026-01-05 16:42:19 +0800

    feat(release): print cwd

--- setup-bun/publish.sh
+++ setup-bun/publish.sh
@@ -1,5 +1,5 @@
 # As Bun does not support `publish --recursive`, we need to manually publish each package.
 for package in "$@"; do
   # (cd $package && bun publish --access public)
-  echo $package
+  cd $package && echo "$(cwd)"
 done
