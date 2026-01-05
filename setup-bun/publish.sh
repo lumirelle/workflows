546664e6430e2ede321abde36e33f6500aa21249
Lumirelle <shabbyacc@outlook.com>
2026-01-05 15:45:08 +0800

    feat(release): try better support of monorepo publishing

---
+++ setup-bun/publish.sh
@@ -0,0 +1,5 @@
+# As Bun does not support `publish --recursive`, we need to manually publish each package.
+for package in "$(ls $1)"; do
+  # (cd $package && bun publish --access public)
+  echo $package
+done
