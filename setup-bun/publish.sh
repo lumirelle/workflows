575211a24ea4842b770e1ab8ef4b862c224a2e19
Lumirelle <shabbyacc@outlook.com>
2026-01-06 17:11:38 +0800

    feat(release)!: use inline bash script for better security

--- setup-bun/publish.sh
+++
@@ -1,4 +0,0 @@
-# As Bun does not support `publish --recursive`, we need to manually publish each package.
-for package in "$@"; do
-  (cd $package  && bun pm pack --filename "${package}/release.tgz" && bunx npm publish ./release.tgz --access public)
-done
