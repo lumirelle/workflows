512425fde449fbddc59e9843640f5cd3208a6fe5
Lumirelle <shabbyacc@outlook.com>
2026-03-05 10:39:00 +0800

    chore: speed up script running, update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,35 @@
 // @ts-check
-import antfu from '@antfu/eslint-config'
+import { antfu } from '@antfu/eslint-config'
 import oxlint from 'eslint-plugin-oxlint'
 
 export default antfu(
   {
-    type: 'lib',
     stylistic: false,
-    ignores: ['test/exports/**'],
+    type: 'lib',
   },
-  ...oxlint.configs['flat/recommended'],
-)
+  ...oxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+).override('antfu/perfectionist/setup', {
+  rules: {
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        environment: 'bun',
+        groups: [
+          'type-import',
+          ['type-parent', 'type-sibling', 'type-index', 'type-internal'],
+          'value-builtin',
+          'value-external',
+          'value-internal',
+          ['value-parent', 'value-sibling', 'value-index'],
+          'side-effect',
+          'ts-equals-import',
+          'unknown',
+        ],
+        newlinesBetween: 'ignore',
+        newlinesInside: 'ignore',
+        order: 'asc',
+        type: 'natural',
+      },
+    ],
+  },
+})
