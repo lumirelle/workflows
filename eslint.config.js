ea8f84f981e2e2c4a1d5da3e6d0479108630fd1e
Lumirelle <shabbyacc@outlook.com>
2026-05-24 22:54:34 +0800

    chore: move eslint perfectionist plugin rule overrides to options

--- eslint.config.js
+++ eslint.config.js
@@ -5,30 +5,31 @@ import oxlint from 'eslint-plugin-oxlint'
 export default antfu(
   {
     type: 'lib',
+    perfectionist: {
+      overrides: {
+        'perfectionist/sort-imports': [
+          'error',
+          {
+            environment: 'bun',
+            groups: [
+              'type-import',
+              ['type-parent', 'type-sibling', 'type-index', 'type-internal'],
+              'value-builtin',
+              'value-external',
+              'value-internal',
+              ['value-parent', 'value-sibling', 'value-index'],
+              'side-effect',
+              'ts-equals-import',
+              'unknown',
+            ],
+            newlinesBetween: 'ignore',
+            newlinesInside: 'ignore',
+            order: 'asc',
+            type: 'natural',
+          },
+        ],
+      },
+    },
   },
   ...oxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
-).override('antfu/perfectionist/setup', {
-  rules: {
-    'perfectionist/sort-imports': [
-      'error',
-      {
-        environment: 'bun',
-        groups: [
-          'type-import',
-          ['type-parent', 'type-sibling', 'type-index', 'type-internal'],
-          'value-builtin',
-          'value-external',
-          'value-internal',
-          ['value-parent', 'value-sibling', 'value-index'],
-          'side-effect',
-          'ts-equals-import',
-          'unknown',
-        ],
-        newlinesBetween: 'ignore',
-        newlinesInside: 'ignore',
-        order: 'asc',
-        type: 'natural',
-      },
-    ],
-  },
-})
+)
