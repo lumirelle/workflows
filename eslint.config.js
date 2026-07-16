903b1d2b0b9fc2086abfb135cc3f7509795718f9
Lumirelle <shabbyacc@outlook.com>
2026-07-16 15:06:49 +0800

    chore: remove bun specific eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -6,33 +6,6 @@ export default antfu(
   {
     // Strict lib type
     type: 'lib',
-
-    // Overrides
-    perfectionist: {
-      overrides: {
-        'perfectionist/sort-imports': [
-          'error',
-          {
-            environment: 'bun',
-            groups: [
-              'type-import',
-              ['type-parent', 'type-sibling', 'type-index', 'type-internal'],
-              'value-builtin',
-              'value-external',
-              'value-internal',
-              ['value-parent', 'value-sibling', 'value-index'],
-              'side-effect',
-              'ts-equals-import',
-              'unknown',
-            ],
-            newlinesBetween: 'ignore',
-            newlinesInside: 'ignore',
-            order: 'asc',
-            type: 'natural',
-          },
-        ],
-      },
-    },
     toml: {
       overrides: {
         'toml/array-element-newline': ['error', 'consistent'],
